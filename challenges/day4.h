9268dc293224be21bec6bbc4c832203edc0b5f84
Michael <theos.michael@gmail.com>
2020-12-04 19:46:39 +1100

    day 4

---
+++ challenges/day4.h
@@ -0,0 +1,11 @@
+//
+// Created by Michael on 04/12/2020.
+//
+
+#ifndef DAY4_H
+#define DAY4_H
+
+void passports1();
+void passports2();
+
+#endif //ADVENT_OF_CODE_2020_DAY4_H
--- main.c
+++ main.c
@@ -1,13 +1,25 @@
+#include <stdio.h>
 #include "challenges/day1.h"
 #include "challenges/day2.h"
 #include "challenges/day3.h"
+#include "challenges/day4.h"
 
 int main() {
+    printf("expenses1:\n");
     expenses1();
+    printf("\nexpenses2:\n");
     expenses2();
+    printf("\npasswords1:\n");
     passwords1();
+    printf("\npasswords2:\n");
     passwords2();
+    printf("\ntrees1:\n");
     trees1();
+    printf("\ntrees2:\n");
     trees2();
+    printf("\npassports1:\n");
+    passports1();
+    printf("\npassports2:\n");
+    passports2();
     return 0;
 }
